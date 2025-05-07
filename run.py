c40eebd83a66a499cfbf75e5686a5af11711ce5d
friend25kr <friend25kr@gmail.com>
2025-05-07 17:07:01 +0900

    my fisrt commit

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 list_training_data = list(training_data)
 list_test_data = list(test_data)
 
-net = network.Network([784, 30, 10])
+net = network.Network([784, 100, 10])
 net.SGD(list_training_data, 30, 10, 3.0, test_data=list_test_data)
