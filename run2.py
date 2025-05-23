674abd7e04c8b4351901c3db09cc14e3112875ae
friend25kr <friend25kr@gmail.com>
2025-05-23 10:59:29 +0900

    Add files via upload

---
+++ run2.py
@@ -0,0 +1,12 @@
+import mnist_loader
+import network2
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+list_training_data = list(training_data)
+list_test_data = list(test_data)
+
+print(list_test_data[500][0], list_test_data[500][1])
+
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(list_training_data, 30, 10, 0.5, evaluation_data=list_test_data, monitor_evaluation_accuracy=True)
