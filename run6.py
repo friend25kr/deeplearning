09746092fbe5917fcdabe7a112a00d5dd4a924f8
friend25kr <friend25kr@gmail.com>
2025-06-26 09:02:07 +0900

    Add files via upload

--- run6.py
+++ run6.py
@@ -1,4 +1,3 @@
-import mnist_loader
 import network3
 
 from network3 import Network
@@ -6,9 +5,6 @@ from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 
 training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
-list_training_data = list(training_data)
-list_test_data = list(test_data)
-list_validation_data = list(validation_data)
 
 net = Network([FullyConnectedLayer(n_in=784, n_out=100), SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
 net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
